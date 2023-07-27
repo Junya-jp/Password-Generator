11fa8c2d119aa6a91c2ce6d2faf9974176aaa825
Junior Poku <juniorpoku@Juniors-MacBook-Pro.local>
2023-07-27 11:21:37 -0400

    Added Prompt for user to select character length

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,20 @@
 // Assignment code here
-
+//Global variables for function
+var lowercase = ["abcdefghijklmnopqrstuvwxyz"];
+var uppercase =["ABCDEFGHIJKLMNOPQRSTUVWXYZ"];
+var num = ["0123456789"];
+var special_chars= ["!@#$%^&*()_"];
+var passwordlength = []
+//for loop to add numbers 8 to 128 to passwordlength
+for (var i =  8; i<= 128; i++) {
+  passwordlength.push(i);
+}
+function generatePassword() {
+  var passwordChar ='';
+  //variable to hold value of user selection
+  var selectlength = parseInt(prompt("Select length of password (8-128) characters"));
+  
+}
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
